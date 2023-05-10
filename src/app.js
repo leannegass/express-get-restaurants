66014280afe1c85020a6968033f1160f6ec4ef5b
Leanne Gass <leannegass@gmail.com>
2023-05-10 10:40:29 +0100

    added GET /restaurants/:id endpoit

--- src/app.js
+++ src/app.js
@@ -6,9 +6,16 @@ const db = require("../db/connection");
 //TODO: Create your GET Request Route Below: 
 app.set("json spaces", 2);
 
-app.get("/restaurants",  async (Request, response, next)=>{
+app.get("/restaurants",  async (request, response, next)=>{
     const restaurants = await Restaurant.findAll();
     response.json(restaurants);
+})
+
+app.get("/restaurants/:id", async (request, response, next) => {
+    const index = request.params.id 
+    const restaurantAtIndex = await Restaurant.findByPk(index);
+    response.json(restaurantAtIndex);
+    
 })
 
 //app.use()
