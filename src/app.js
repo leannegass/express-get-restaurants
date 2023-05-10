6cf4c47584bfae43aab6567d3c4ab2ea6c1e0809
Leanne Gass <leannegass@gmail.com>
2023-05-10 11:28:20 +0100

    added PUT, DELETE routes to update or delete restaurants from database

--- src/app.js
+++ src/app.js
@@ -5,20 +5,46 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 app.set("json spaces", 2);
+app.use(express.json()); //parse the request body that contain JSON
+//app.use(express.urlencoded()); // parse the request body with urlencoded values
 
 app.get("/restaurants",  async (request, response, next)=>{
     const restaurants = await Restaurant.findAll();
     response.json(restaurants);
-})
+});
 
 app.get("/restaurants/:id", async (request, response, next) => {
     const index = request.params.id 
     const restaurantAtIndex = await Restaurant.findByPk(index);
     response.json(restaurantAtIndex);
     
+});
+// express route to add a new restaurant to database
+
+app.post("/restaurants", async (request, response, next) => {
+    const restaurant = request.body;
+    await Restaurant.create(restaurant);
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+})
+// express route to update(replace) a restaurant in database
+app.put("/restaurants/:id", async (request, response, next) => {
+    const index = request.params.id 
+    const restaurantAtIndex = await Restaurant.findByPk(index);
+    const newRestaurant = request.body;
+    await restaurantAtIndex.update(newRestaurant);
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
 })
 
-//app.use()
+// express route to delete a restaurant in given index in database
+app.delete("/restaurants/:id", async (request, response, next) => {
+    const index = request.params.id 
+    const restaurantAtIndex = await Restaurant.findByPk(index);
+    await restaurantAtIndex.destroy();
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+})
 
 
 
